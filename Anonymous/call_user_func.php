0e32931d5ee5a9be79cd0a47f63a23d7b4dedcfc
Matsu <matsumoto@donow.jp>
2017-06-21 10:03:31 +0900

    複数パラメータ

--- Anonymous/call_user_func.php
+++ Anonymous/call_user_func.php
@@ -1,17 +1,19 @@
 <?php
 
+namespace Calculation;
+
 class Calc{
 
-    private function getName($param) {
-        return $param['name'];
+    private function getName($param, $param2) {
+        return $param['first_name'] . " " . $param2['family_name'];
     }
 
-    private function getAge($param) {
-        return $param['age'];
+    private function getAge($param, $param2) {
+        return $param['age'] . " " . $param2['birth_day'];
     }
 
-    public function doFunc($param, $method) {
-        $res = call_user_func(array($this, 'get'. $method), $param);
+    public function doFunc($param, $param2,$method) {
+        $res = call_user_func(array($this, 'get'. $method), $param, $param2);
         echo $res;
     }
 }
@@ -19,7 +21,10 @@ class Calc{
 $calc = new Calc();
 
 $calc->doFunc([
-    'name'  => 'tarou',
+    'first_name'  => 'tarou',
     'age'   => 30
+],[
+    'family_name' => 'matsumoto',
+    'birth_day'   => '1985/1/1'
 ], 'Age');
-//30
+//30  1985/1/1
