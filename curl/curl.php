432cb740b5978d9c8e6a4d55b8c608401387fc8a
Norio <umanari145@gmail.com>
2018-08-16 00:50:50 +0900

    http code出力

--- curl/curl.php
+++ curl/curl.php
@@ -11,17 +11,28 @@ $ch = curl_init();
 
 curl_setopt_array($ch, [
     CURLOPT_URL => POST_URL,
+    //trueにするとcurl_exeの返り値が文字列
     CURLOPT_RETURNTRANSFER => true,
+    //postメソッド
     CURLOPT_POST => true,
+    //http response
     CURLOPT_HEADER => true,
+    //postのデータをエンコーディングして引き渡す
     CURLOPT_POSTFIELDS => http_build_query($post_data),
 ]);
 
 $response = curl_exec($ch);
+
+//ステータスコード
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+var_dump($code);
+
+
 $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
 $header = substr($response, 0, $header_size);
-//response出力
+//httpヘッダー出力
 var_dump($header);
+
 //responseの形を出力
 var_dump($response);
 
