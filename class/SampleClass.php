dcfbab8259790b3012202993a27cfc940c325579
Norio <umanari145@gmail.com>
2019-04-29 01:09:23 +0900

    静的変数のエラーを定義

--- class/SampleClass.php
+++ class/SampleClass.php
@@ -19,32 +19,43 @@ $scb->instanceMethod2();//1;
 $sca->instanceMethod2();//2;
 $scb->instanceMethod2();//2;
 
+//静的変数なので呼べる
+SampleClass::$num1;
+//インスタンス変数なので呼べない
+//エラーが起こる
+//SampleClass::$num2;
 
 class SampleClass
 {
     //クラス・インスタンス間で共有される
-    private static $num1 ;
+    public static $num1 ;
 
     //特定インスタンスのみ
-    private $num2;
+    public $num2;
 
-    public static function staticMethod1(){
+    public static function staticMethod1()
+    {
         $num = self::$num1;
         $num++;
         self::$num1 = $num;
         echo $num .PHP_EOL;
+
         //下記はエラーになる。staticメソッドからインスタンス変数は呼べない(クラスからインスタンスは呼べない)
         //echo $this->num2;
+        //下記も当然static変数ではないので呼べない
+        //echo self::$num2;
 
         //クラスメソッドからインスタンスメソッドはよべない エラーになる
         //$this->instanceMethod1();
     }
 
-    public static function staticMethod2(){
+    public static function staticMethod2()
+    {
         echo 'static method2'. PHP_EOL;
     }
 
-    public function instanceMethod1(){
+    public function instanceMethod1()
+    {
         //インスタンスメソッドからクラス変数は呼べる
         $num = self::$num1;
         $num++;
@@ -61,12 +72,12 @@ class SampleClass
         $this->staticMethod2();
     }
 
-    public function instanceMethod2(){
+    public function instanceMethod2()
+    {
         //インスタンスメソッドからクラス変数は呼べる(通常の使い方)
         $num = $this->num2;
         $num++;
         $this->num2 = $num;
         echo $num .PHP_EOL;
     }
-
 }
