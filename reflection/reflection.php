0f06f2f026ff59a1b097579ac34bbcb6546cca05
Norio <umanari145@gmail.com>
2020-05-07 23:31:21 +0900

    もっと簡便なリフレクション

--- reflection/reflection.php
+++ reflection/reflection.php
@@ -9,9 +9,24 @@ $classes = [
     'animal\Dog'
 ];
 
+
+echo "reflection \n";
+
 foreach ($classes as $class) {
     $reflClass = new \ReflectionClass($class);
     $animal = $reflClass->newInstance();
     $animal->bark() ;
+    echo "\n";
+}
+
+echo "not reflection \n";
+
+/**
+ *  動的なクラスやメソッド定義だけなら下記のような書き方だけでもいいかも
+ */
+$func = 'bark';
+foreach ($classes as $class) {
+    $animalClass = new $class();
+    $animalClass->{$func}();
     echo "\n";
 }
