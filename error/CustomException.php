73b5030cd92ff0f78aa44d1509ccfee8cf0770e7
umanari145 <umanari145@gmail.com>
2021-02-13 22:34:04 +0900

    formatの修正

--- error/CustomException.php
+++ error/CustomException.php
@@ -1,19 +1,23 @@
 <?php
 
-class CustomException extends Exception{
+class CustomException extends Exception
+{
 
     private $errorCode;
 
-    public function __construct(Exception $e) {
+    public function __construct(Exception $e)
+    {
         $this->e = $e;
     }
 
-    public function getCustomErrorCode () {
+    public function getCustomErrorCode()
+    {
         $this->errorCode = 'E1111';
         return $this->errorCode;
     }
 
-    public function getCustomMessage() {
+    public function getCustomMessage()
+    {
         return sprintf("%s%s", $this->e->getMessage(), $this->e->getTraceAsString());
     }
 }
