8363ff0aa6c5320862cb5b527efc6cc3ea526395
umanari145 <umanari145@gmail.com>
2023-04-10 22:58:29 +0900

    いろいろな演算子を追加

--- php8/operator.php
+++ php8/operator.php
@@ -7,33 +7,28 @@ echo "\n";
 echo "--エルビス演算子-- emptyのように幅広い表示";
 echo "\n";
 // エルビス演算子
-$var1 = 'a';
-echo $var1 ?: '' ;
 
-$var2 = '';
-echo $var2 ?: '空だとこっちが表示' ;
+//当然aが表示される
+echo 'a' ?: '' ;
+
+echo '' ?: '空だとこっちが表示' ;
 echo "\n";
 
-$var3 = false;
-echo $var3 ?: 'falseだとこっちが表示' ;
+echo false ?: 'falseだとこっちが表示' ;
 echo "\n";
 
-$var4 = 0;
-echo $var4 ?: '0だとこっちが表示' ;
+echo 0 ?: '0だとこっちが表示' ;
 echo "\n";
 
-
-$var5 = null;
-echo $var5 ?: 'nullだとこっちが表示' ;
+echo null ?: 'nullだとこっちが表示' ;
 echo "\n";
 
 echo "--こっからnull演算子-- nullのみ";
 echo "\n";
 //null演算子
-$nullable_var1 = null;
-echo $nullable_var1 ?? 'nullだとこっちが表示' ;
+echo null ?? 'nullだとこっちが表示' ;
 echo "\n";
 
-$nullable_var2 = '空白だと??の右は表示されない';
-echo $nullable_var2 ?? 'だめぽ' ;
+//'空白だと??の右は表示されない';
+echo '' ?? 'だめぽ' ;
 echo "\n";
