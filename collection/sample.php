e42990d970769a0b9028f6cadfedaa2f0377b592
umanari145 <umanari145@gmail.com>
2023-07-16 00:09:24 +0900

    collectionのサンプルを記述

--- collection/sample.php
+++ collection/sample.php
@@ -15,384 +15,42 @@ $arr = [
     ["name" => "jirou ",  "domain" => "hotmail.com", "age" => "9", "pref" => "tokyo"]
 ];
 
+$arr = collect($arr);
 #抽出
-#条件
-/*$arr2 = collect($arr)
-    ->filter(fn($ele) => $ele['age'] >= 20);
-var_dump($arr2);
-*/
-$colors = ['red', 'green', 'blue'];
-$collection = collect($colors);
+#条件 20歳以上を抽出
+$arr2 = $arr->filter(fn($ele) => $ele['age'] >= 20);
+//dd($arr2);
 
-$updatedColors = $collection->map(function ($item, $key) {
-    return 'color '. $item;
-});
+#where句 (filterとほぼ同じ)
+$arr3 = $arr->where('pref', 'chiba');
 
-var_dump($updatedColors->all());
-exit();
-//var_dump( $arr2 );
-//array(3) {
-//  [0]=>
-//  array(4) {
-//    ["name"]=>
-//    string(7) " kazumi"
-//    ["domain"]=>
-//    string(9) "gmail.com"
-//    ["age"]=>
-//    string(2) "30"
-//    ["pref"]=>
-//    string(5) "chiba"
-//  }
-//  [2]=>
-//  array(4) {
-//    ["name"]=>
-//    string(8) " yuusuke"
-//    ["domain"]=>
-//    string(11) "hotmail.com"
-//    ["age"]=>
-//    string(2) "25"
-//    ["pref"]=>
-//    string(5) "chiba"
-//  }
-//  [3]=>
-//  array(4) {
-//    ["name"]=>
-//    string(8) " satoshi"
-//    ["domain"]=>
-//    string(9) "gmail.com"
-//    ["age"]=>
-//    string(2) "45"
-//    ["pref"]=>
-//    string(8) "kanagawa"
-//  }
-//}
-
-#where句
-//$arr2 = _::where($arr, ['pref' => "chiba"]);
-//var_dump( $arr2 );
-//array(2) {
-//  [0]=>
-//  array(4) {
-//    ["name"]=>
-//    string(7) " kazumi"
-//    ["domain"]=>
-//    string(9) "gmail.com"
-//    ["age"]=>
-//    string(2) "30"
-//    ["pref"]=>
-//    string(5) "chiba"
-//  }
-//  [2]=>
-//  array(4) {
-//    ["name"]=>
-//    string(8) " yuusuke"
-//    ["domain"]=>
-//    string(11) "hotmail.com"
-//    ["age"]=>
-//    string(2) "25"
-//    ["pref"]=>
-//    string(5) "chiba"
-//  }
-//}
-
-//$arr2 = _::pluck($arr, 'domain');
-//var_dump( $arr2 );
-//array(5) {
-//  [0]=>
-//  string(9) "gmail.com"
-//  [1]=>
-//  string(11) "yahoo.co.jp"
-//  [2]=>
-//  string(11) "hotmail.com"
-//  [3]=>
-//  string(9) "gmail.com"
-//  [4]=>
-//  string(11) "hotmail.com"
-//}
-
+#特定プロパティのみを抽出
+$arr4 = $arr->pluck($arr, 'name');
 
 #集計
+#県ごとにグルーピングgroupby
+$arr5 = $arr->groupBy('pref');
 
-#県ごとにグルーピングgroupby 
-//$arr2 = _::groupBy( $arr, function($ele) { return $ele["pref"]; } );
-
-//var_dump( $arr2 );
-
-//県ごとにグルーピング
-//array(3) {
-//  ["chiba"]=>
-//  array(2) {
-//    [0]=>
-//    array(4) {
-//      ["name"]=>
-//      string(7) " kazumi"
-//      ["domain"]=>
-//      string(9) "gmail.com"
-//      ["age"]=>
-//      string(2) "30"
-//      ["pref"]=>
-//      string(5) "chiba"
-//    }
-//    [1]=>
-//    array(4) {
-//      ["name"]=>
-//      string(8) " yuusuke"
-//      ["domain"]=>
-//      string(11) "hotmail.com"
-//      ["age"]=>
-//      string(2) "25"
-//      ["pref"]=>
-//      string(5) "chiba"
-//    }
-//  }
-//  ["tokyo"]=>
-//  array(2) {
-//    [0]=>
-//    array(4) {
-//      ["name"]=>
-//      string(7) "ichirou"
-//      ["domain"]=>
-//      string(11) "yahoo.co.jp"
-//      ["age"]=>
-//      string(2) "18"
-//      ["pref"]=>
-//      string(5) "tokyo"
-//    }
-//    [1]=>
-//    array(4) {
-//      ["name"]=>
-//      string(6) "jirou "
-//      ["domain"]=>
-//      string(11) "hotmail.com"
-//      ["age"]=>
-//      string(1) "9"
-//      ["pref"]=>
-//      string(5) "tokyo"
-//    }
-//  }
-//  ["kanagawa"]=>
-//  array(1) {
-//    [0]=>
-//    array(4) {
-//      ["name"]=>
-//      string(8) " satoshi"
-//      ["domain"]=>
-//      string(9) "gmail.com"
-//      ["age"]=>
-//      string(2) "45"
-//      ["pref"]=>
-//      string(8) "kanagawa"
-//    }
-//  }
-//}
-//
-//https://quartet-communications.com/info/topics/17506
+//年齢別の集計
+$arr6 = $arr->countBy(fn($ele) => $ele['age'] >= 20 ? 'child' : 'adult');
+/*
+^ Illuminate\Support\Collection^ {#12
+  #items: array:2 [
+    "child" => 3
+    "adult" => 2
+  ]
+  #escapeWhenCastingToString: false
+}*/
 
 
-//重複がないパターンは使える
-//$arr3 = _::indexBy( $arr , 'pref');
-
-//var_dump( $arr3 );
-
-//array(3) {
-//  ["chiba"]=>
-//  array(4) {
-//    ["name"]=>
-//    string(8) " yuusuke"
-//    ["domain"]=>
-//    string(11) "hotmail.com"
-//    ["age"]=> 
-//    string(2) "25"
-//    ["pref"]=>
-//    string(5) "chiba"
-//  }
-//  ["tokyo"]=>
-//  array(4) {
-//    ["name"]=>
-//    string(6) "jirou "
-//    ["domain"]=>
-//    string(11) "hotmail.com"
-//    ["age"]=>
-//    string(1) "9"
-//    ["pref"]=>
-//    string(5) "tokyo"
-//  }
-//  ["kanagawa"]=>
-//  array(4) {
-//    ["name"]=>
-//    string(8) " satoshi"
-//    ["domain"]=>
-//    string(9) "gmail.com"
-//    ["age"]=>
-//    string(2) "45"
-//    ["pref"]=>
-//    string(8) "kanagawa"
-//  }
-//}
-//
-//
-//県別の数量
-
-/*$arr4 = _::countBy( $arr ,function( $ele ){ 
-    return ( $ele["age"]  >= 20 ) ? "child": " adult ";
-} );*/
-
-//var_dump( $arr4 );
-
-//array(2) {
-//  ["child"]=>
-//  int(3)
-//  [" adult "]=>
-//  int(2)
-//}
-//
-//
 //少し複雑なパターン
-/*$arr5 = _::countBy( $arr ,function( $ele ){ 
-    $age; 
-    if( $ele["age"]  < 10 ){
-        $age ="jidou";
-    }elseif( $ele["age"] < 20 ){
-        $age ="teen";
-    }elseif( $ele["age"] < 30 ){
-        $age ="twenty";
-    }else{
-        $age ="adult";
-    }
-    return $age;
-} );*/
-
-//var_dump( $arr5 );
-//array(4) {
-//  ["adult"]=>
-//  int(2)
-//  ["teen"]=>
-//  int(1)
-//  ["twenty"]=>
-//  int(1)
-//  ["jidou"]=>
-//  int(1)
-//}
-//
 //名前、メルアド(空白除去＋ドメイン)の配列を作る
-$arr2 = _::map( $arr, function($ele) {  
-    $ele["email"] = trim( $ele["name"] ) ."@". $ele["domain"];
-    return $ele;
-});
-
-//var_dump( $arr2 );
-//array(5) {
-//  [0]=>
-//  array(5) {
-//    ["name"]=>
-//    string(7) " kazumi"
-//    ["domain"]=>
-//    string(9) "gmail.com"
-//    ["age"]=>
-//    string(2) "30"
-//    ["pref"]=>
-//    string(5) "chiba"
-//    ["email"]=>
-//    string(16) "kazumi@gmail.com"
-//  }
-//  [1]=>
-//  array(5) {
-//    ["name"]=>
-//    string(7) "ichirou"
-//    ["domain"]=>
-//    string(11) "yahoo.co.jp"
-//    ["age"]=>
-//    string(2) "18"
-//    ["pref"]=>
-//    string(5) "tokyo"
-//    ["email"]=>
-//    string(19) "ichirou@yahoo.co.jp"
-//  }
-//  [2]=>
-//  array(5) {
-//    ["name"]=>
-//    string(8) " yuusuke"
-//    ["domain"]=>
-//    string(11) "hotmail.com"
-//    ["age"]=>
-//    string(2) "25"
-//    ["pref"]=>
-//    string(5) "chiba"
-//    ["email"]=>
-//    string(19) "yuusuke@hotmail.com"
-//  }
-//  [3]=>
-//  array(5) {
-//    ["name"]=>
-//    string(8) " satoshi"
-//    ["domain"]=>
-//    string(9) "gmail.com"
-//    ["age"]=>
-//    string(2) "45"
-//    ["pref"]=>
-//    string(8) "kanagawa"
-//    ["email"]=>
-//    string(17) "satoshi@gmail.com"
-//  }
-//  [4]=>
-//  array(5) {
-//    ["name"]=>
-//    string(6) "jirou "
-//    ["domain"]=>
-//    string(11) "hotmail.com"
-//    ["age"]=>
-//    string(1) "9"
-//    ["pref"]=>
-//    string(5) "tokyo"
-//    ["email"]=>
-//    string(17) "jirou@hotmail.com"
-//  }
-//}
-//
-//
-//
+$arr7 = $arr->map(fn($ele) => sprintf('%s@%s', $ele['name'], $ele['domain']));
 
 
 #30歳以上のメルアド(名前の空白を削除)を作る
-/*$arr3 = _::chain($arr)
-    ->filter( function( $ele ) { return $ele["age"] >= 30; })
-    ->map( function( $ele ) { 
-        $ele["email"] = trim( $ele["name"] ) ."@". $ele["domain"];
+$arr8 = $arr->filter(fn($ele) => $ele['age'] >= 30)
+    ->map(function ($ele) {
+        $ele['email'] = sprintf('%s@%s', trim($ele['name']), $ele['domain']);
         return $ele;
     });
-
-var_dump( $arr3 );
-*/
-//object(Underbar\Internal\Wrapper)#2 (2) {
-//  ["value":"Underbar\Internal\Wrapper":private]=>
-//  array(2) {
-//    [0]=>
-//    array(5) {
-//      ["name"]=>
-//      string(7) " kazumi"
-//      ["domain"]=>
-//      string(9) "gmail.com"
-//      ["age"]=>
-//      string(2) "30"
-//      ["pref"]=>
-//      string(5) "chiba"
-//      ["email"]=>
-//      string(16) "kazumi@gmail.com"
-//    }
-//    [3]=>
-//    array(5) {
-//      ["name"]=>
-//      string(8) " satoshi"
-//      ["domain"]=>
-//      string(9) "gmail.com"
-//      ["age"]=>
-//      string(2) "45"
-//      ["pref"]=>
-//      string(8) "kanagawa"
-//      ["email"]=>
-//      string(17) "satoshi@gmail.com"
-//    }
-//  }
-//  ["impl":"Underbar\Internal\Wrapper":private]=>
-//  string(18) "Underbar\ArrayImpl"
-//}
